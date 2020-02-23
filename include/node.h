827ed48f68568a6e055a8d0b5fb63a2e92b5cb18
kikeXD <kike@gamil.com>
2020-02-23 00:56:54 -0500

    adding some constructors to node.h

--- include/node.h
+++ include/node.h
@@ -7,6 +7,33 @@ struct node
     T value;
     node<T> *next;
     node<T> *prev;
+    node();
+    node(T);
+    node(T&);
+    void operator=(T);
 };
+
+template <typename T>
+node<T>::node()
+{
+}
+
+template <typename T>
+node<T>::node(T t)
+{
+    this->value = t;
+}
+
+template <typename T>
+node<T>::node(T& t)
+{
+    this->value = t;
+}
+
+template <typename T>
+void node<T>::operator=(T t)
+{
+    this->value = t;
+}
 
 #endif //NODE_H
